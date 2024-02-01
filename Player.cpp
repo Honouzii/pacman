6e247cf1224937f85944ce8ffa3f701292d43033
侑飛 松浦 <230419@jc-21.jp>
2024-02-01 11:35:35 +0900

    なんかちょっと書いた

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,11 @@
 #include "Player.h"
 #include "Engine/Model.h"
 #include "Engine/Input.h"
+#include "Engine/Debug.h"
+
+namespace {
+	const float PLAYER_MOVE_SPEED{ 1.0f };
+}
 
 Player::Player(GameObject* parent)
 {
@@ -17,23 +22,26 @@ void Player::Initialize()
 void Player::Update()
 {
 	XMVECTOR vFront = { 0,0,1,0 };
-	XMVECTOR move{ 0,0,1,0 };
-	float speed = 0.01f;
+	XMVECTOR move{ 0,0,0,0 };
 
-	if (Input::IsKey(DIK_UP)) {
+	if (Input::IsKeyDown(DIK_UP)) {
 		move = XMVECTOR{ 0,0,1,0 };
 	}
-	if (Input::IsKey(DIK_DOWN)) {
+	if (Input::IsKeyDown(DIK_DOWN)) {
 		move = XMVECTOR{ 0,0,-1,0 };
 	}
-	if (Input::IsKey(DIK_LEFT)) {
+	if (Input::IsKeyDown(DIK_LEFT)) {
 		move = XMVECTOR{ -1,0,0,0 };
 	}
-	if (Input::IsKey(DIK_RIGHT)) {
+	if (Input::IsKeyDown(DIK_RIGHT)) {
 		move = XMVECTOR{ 1,0,0,0 };
 	}
 	XMVECTOR pos = XMLoadFloat3(&(transform_.position_));
-	pos = pos + speed * move;
+	pos = pos + PLAYER_MOVE_SPEED * move;
+	Debug::Log("(x,y=");
+	Debug::Log(XMVectorGetX(pos));
+	Debug::Log(",");
+	Debug::Log(XMVectorGetX(pos), true);
 	XMStoreFloat3(&(transform_.position_), pos);
 
 	XMVECTOR vdot = XMVector3Dot(vFront, move);
